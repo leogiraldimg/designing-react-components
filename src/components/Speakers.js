c7ac03eda0df896f5ab6fee10c89d09a654a01df
Leonardo Giraldi <leogiraldimg@gmail.com>
2021-12-23 15:40:46 -0300

    feat: lift state for sharing between components

--- src/components/Speakers.js
+++ src/components/Speakers.js
@@ -1,13 +1,20 @@
+import { useState } from 'react';
 import { data } from '../../SpeakerData';
 import Header from './Header';
 import SpeakersList from './SpeakersList';
 import SpeakersToolbar from './SpeakersToolbar';
 
 function Speakers() {
+  const [theme, setTheme] = useState('light');
+
   return (
-    <div className="container-fluid">
-      <Header />
-      <SpeakersToolbar />
+    <div
+      className={
+        theme === 'light' ? 'container-fluid light' : 'container-fluid dark'
+      }
+    >
+      <Header theme={theme} />
+      <SpeakersToolbar theme={theme} setTheme={setTheme} />
       <SpeakersList data={data} />
     </div>
   );
