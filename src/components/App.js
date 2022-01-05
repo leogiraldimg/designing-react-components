fa6f69f476605932d941b8c4bd7762af91c01bff
Leonardo Giraldi <leogiraldimg@gmail.com>
2022-01-05 17:34:29 -0300

    ✨ introduce layout component

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,15 @@
-import { createContext, useState } from 'react';
 import Header from './Header';
 import Speakers from './Speakers';
-
-export const ThemeContext = createContext();
+import Layout from './Layout';
 
 function App() {
-  const [theme, setTheme] = useState('light');
-
   return (
-    <ThemeContext.Provider value={{ setTheme, theme }}>
-      <div
-        className={
-          theme === 'light' ? 'container-fluid light' : 'container-fluid dark'
-        }
-      >
+    <Layout startingTheme="light">
+      <div>
         <Header />
         <Speakers />
       </div>
-    </ThemeContext.Provider>
+    </Layout>
   );
 }
 
