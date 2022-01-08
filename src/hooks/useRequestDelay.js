4df052dc9af521f2a3eef8cf4852a52d17d55063
Leonardo Giraldi <leogiraldimg@gmail.com>
2022-01-08 00:57:43 -0300

    :sparkles: add insert delete to request context

--- src/hooks/useRequestDelay.js
+++ src/hooks/useRequestDelay.js
@@ -31,10 +31,10 @@ function useRequestDelay(delayTime = 1000, initialData = []) {
     delayFunc();
   }, []);
 
-  function updateRecord(recordUpdated, doneCallback) {
+  function updateRecord(record, doneCallback) {
     const originalRecords = [...data];
     const newRecords = data.map(function (rec) {
-      return rec.id === recordUpdated.id ? recordUpdated : rec;
+      return rec.id === record.id ? record : rec;
     });
 
     async function delayFunction() {
@@ -56,7 +56,62 @@ function useRequestDelay(delayTime = 1000, initialData = []) {
     delayFunction();
   }
 
-  return { data, requestStatus, error, updateRecord };
+  function insertRecord(record, doneCallback) {
+    const originalRecords = [...data];
+    const newRecords = [record, ...data];
+
+    async function delayFunction() {
+      try {
+        setData(newRecords);
+        await delay(delayTime);
+        if (doneCallback) {
+          doneCallback();
+        }
+      } catch (error) {
+        console.log('error thrown inside delayFunction', error);
+        if (doneCallback) {
+          doneCallback();
+        }
+        setData(originalRecords);
+      }
+    }
+
+    delayFunction();
+  }
+
+  function deleteRecord(record, doneCallback) {
+    const originalRecords = [...data];
+    const newRecords = data.filter(function (rec) {
+      return rec.id != record.id;
+    });
+
+    async function delayFunction() {
+      try {
+        setData(newRecords);
+        await delay(delayTime);
+        if (doneCallback) {
+          doneCallback();
+        }
+      } catch (error) {
+        console.log('error thrown inside delayFunction', error);
+        if (doneCallback) {
+          doneCallback();
+        }
+        setData(originalRecords);
+      }
+    }
+
+    delayFunction();
+  }
+
+  return {
+    data,
+    requestStatus,
+    error,
+    updateRecord,
+    insertRecord,
+    deleteRecord,
+  };
 }
 
 export default useRequestDelay;
