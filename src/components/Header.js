5140c69e18c6a03fc94364de6ba4a5f25d59364e
leogiraldimg@gmail.com <leogiraldimg@gmail.com>
2022-01-19 23:54:09 -0300

    :sparkles: consuming withAuth HOC

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,42 @@
 import { ThemeContext } from '../contexts/ThemeContext';
 import { useContext } from 'react';
+import withAuth from './withAuth';
 
-function Header() {
+function Header({ loggedInUser, setLoggedInUser }) {
   const { theme } = useContext(ThemeContext);
+
+  function LoggedIn({ loggedInUser, setLoggedInUser }) {
+    return (
+      <div>
+        <span>Logged in as {loggedInUser}</span>
+        <button
+          className="btn btn-secondary"
+          onClick={() => {
+            setLoggedInUser('');
+          }}
+        >
+          Logout
+        </button>
+      </div>
+    );
+  }
+
+  function NotLoggedIn({ loggedInUser, setLoggedInUser }) {
+    return (
+      <div>
+        <button
+          className="btn btn-secondary"
+          onClick={(e) => {
+            e.preventDefault();
+            const username = window.prompt('Enter login name:', '');
+            setLoggedInUser(username);
+          }}
+        >
+          Login
+        </button>
+      </div>
+    );
+  }
 
   return (
     <div className="padT4 padB4">
@@ -15,10 +49,17 @@ function Header() {
             <h4 className="header-title">Silicon Valley Code Camp</h4>
           </div>
           <div className={theme === 'light' ? '' : 'text-info'}>
-            Hello Mr. Smith &nbsp;&nbsp;
-            <span>
-              <a href="#">sign-out</a>
-            </span>
+            {loggedInUser && loggedInUser.length > 0 ? (
+              <LoggedIn
+                loggedInUser={loggedInUser}
+                setLoggedInUser={setLoggedInUser}
+              />
+            ) : (
+              <NotLoggedIn
+                loggedInUser={loggedInUser}
+                setLoggedInUser={setLoggedInUser}
+              />
+            )}
           </div>
         </div>
       </div>
@@ -26,4 +67,4 @@ function Header() {
   );
 }
 
-export default Header;
+export default withAuth(Header);
--- src/components/withAuth.js
+++ src/components/withAuth.js
@@ -14,3 +14,4 @@ function withAuth(Component) {
     );
   };
 }
+export default withAuth;
